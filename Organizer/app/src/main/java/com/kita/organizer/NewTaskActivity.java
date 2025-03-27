eff30012841872ee1f06c706c675eae05b5ba69a
Nikita <NikitaZhebinDev@gmail.com>
2025-03-28 00:07:49 +0100

    Added voice recognition

--- Organizer/app/src/main/java/com/kita/organizer/NewTaskActivity.java
+++ Organizer/app/src/main/java/com/kita/organizer/NewTaskActivity.java
@@ -1,16 +1,42 @@
 package com.kita.organizer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class NewTaskActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_SPEECH_INPUT = 100;
+    private TextView editDate, editTime, textViewSetTime, textViewRepeat;
+    private EditText editTask;
+    private ImageButton imageBtnDate, imageBtnTime, imageBtnSpeak, imgBtnClearDate, imgBtnClearTime, imgBtnNewList;
+    private Button btnRepeat, btnAddToList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +51,116 @@ public class NewTaskActivity extends AppCompatActivity {
             return insets;
         });
 
+        setupToolbar();
+        initButtons();
+        initTextViews();
+    }
+
+    private void initButtons() {
+        imageBtnDate = findViewById(R.id.imageBtnDate);
+        imageBtnTime = findViewById(R.id.imageBtnTime);
+        imgBtnClearDate = findViewById(R.id.imgBtnClearDate);
+        imgBtnClearTime = findViewById(R.id.imgBtnClearTime);
+        imgBtnNewList = findViewById(R.id.imgBtnNewList);
+        btnRepeat = findViewById(R.id.btnRepeat);
+        btnAddToList = findViewById(R.id.btnAddToList);
+        imageBtnSpeak = findViewById(R.id.imageBtnSpeak);
+
+        /* TODO
+        imageBtnDate.setOnClickListener(this);
+        imageBtnTime.setOnClickListener(this);
+        imgBtnClearDate.setOnClickListener(this);
+        imgBtnClearTime.setOnClickListener(this);
+        imgBtnNewList.setOnClickListener(this);
+        btnRepeat.setOnClickListener(this);
+        btnAddToList.setOnClickListener(this);*/
+        imageBtnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVoiceRecognition();
+            }
+        });
+
+        // TODO btnRepeat.setText(DataBaseLists.listRepeatNames[0]);
+        // TODO btnAddToList.setText(DataBaseLists.defaultListsNames[0]);
+    }
+
+    private void initTextViews() {
+        editTask = findViewById(R.id.editTask);
+        textViewSetTime = findViewById(R.id.textViewSetTime);
+        textViewRepeat = findViewById(R.id.textViewRepeat);
+        editDate = findViewById(R.id.editDate);
+        editTime = findViewById(R.id.editTime);
+
+        /* TODO
+        editTask.setOnClickListener(this);
+        editDate.setOnClickListener(this);
+        editTime.setOnClickListener(this);*/
+
+        // Request focus and show keyboard on activity startup
+        editTask.requestFocus();
+        showKeyboard();
+    }
+
+    private void setupToolbar() {
         // remove default toolbar
+        // removed by theme adjustment with no action bar for activity in manifest file
         /*getSupportActionBar().hide();*/
 
         // Set the custom toolbar
         Toolbar toolbar = findViewById(R.id.toolbarNewTask);
         // Set Back button
         toolbar.setNavigationIcon(R.drawable.ic_action_back_arrow);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+        toolbar.setNavigationOnClickListener(v -> {
+            finish();
+        });
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.create_new_task) {
+                // TODO: save task logic
+                Toast.makeText(NewTaskActivity.this, "It worked!", Toast.LENGTH_SHORT).show();
             }
+            return false;
         });
     }
+
+    private void hideKeyboard(@NonNull View v) {
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    private void showKeyboard() {
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
+
+    /**
+     * Voice Recognition via Google Speech API
+     * **/
+    private void startVoiceRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...");
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        } catch (Exception e) {
+            Toast.makeText(this, "Speech recognition is not supported on your device", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Voice Recognition via Google Speech API
+     * **/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK && data != null) {
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (result != null && !result.isEmpty()) {
+                editTask.setText(result.get(0));
+            }
+        }
+    }
+
 }
